0ebbc33031d27e12e49c688fc2a9b813ce0c2cf4
WunmiOmoyeni <omoyeniwunmi@gmail.com>
2024-05-06 13:40:57 -0700

    new

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,22 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../images/logo.svg'
 import togglebutton from '../images/icon-menu.svg'
+import closebutton from '../images/icon-menu-close.svg'
 
 export const Navbar = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const handleDropdownItemClick = (itemName) => {
+    console.log('Clicked on ${itemName}');
+    setIsDropdownOpen(false);
+  }
+
+  const closeDropdown = () => {
+    setIsDropdownOpen(false);
+  }
   return (
-    <div className='navbar'>
+    <div>
+      {isDropdownOpen && <div className='overlay'></div>}
+      <div className='navbar'>
       <nav>
-      <img src={logo}></img>
-      <a>Home</a>
-      <a>New</a>
-      <a>Popular</a>
-      <a>Trending</a>
-      <a>Categories</a>
+      <img src={logo} alt='Logo' className='logo-navbar'></img>
+      <div className='navbar-content'>
+        <a href='#'>Home</a>
+        <a href='#'>New</a>
+        <a href='#'>Popular</a>
+        <a href='#'>Trending</a>
+        <a href='#'>Categories</a>
+      </div> 
+      
+
+        <img src={togglebutton} className='toggle-button' onClick={toggleDropdown}></img>
+      {isDropdownOpen && (
+        <div className='background-blur'>
+          <div className='dropdown-menu'>
+            <img src={closebutton} onClick={closeDropdown}></img>
+          <ul>
+            <li>Home</li>
+            <li>New</li>
+            <li>Popular</li>
+            <li>Trending</li>
+            <li>Categories</li>
+          </ul>
+          </div>
+        </div>
+        
+        
+      )}
+
+
       </nav>
-      <img src={togglebutton} className='toggle-button'></img>
+      
     </div>
+    </div>
+    
   )
 }
 
 
-export default Navbar
+export default Navbar;
