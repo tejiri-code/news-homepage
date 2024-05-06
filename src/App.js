89ac87776c7c7b494146111192aeee15f47c2a51
Edjere Evelyn <evelynedjere@gmail.com>
2024-05-06 16:34:30 +0100

    Remove unused import and fix formatting in App.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 
 import React from 'react'
 import Main from './Components/Main'
-
 import {Routes, Route} from "react-router-dom"
 
-export const App = () => {
+function App() {
   return (
     <Routes>
    <Route path="/" element={<Main/>}/>
